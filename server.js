45c2359e973be690c08f553829d92d246f0a9806
bh@nu77 <bhanupratap04123@gmail.com>
2023-09-22 12:13:44 +0530

    create .env file and set the cloud mongodb url

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ import { MongoClient as Mongodb } from "mongodb";
 import express, { urlencoded } from "express";
 import cors from "cors";
 
-const connString = "mongodb://127.0.0.1:27017";
-const url = "mongodb+srv://bhanupratap04123:ohTeMbMetsHhHYly@cluster0.eth6qf3.mongodb.net/?retryWrites=true&w=majority";
+const connString = "mongodb+srv://bhanupratap04123:XJpEjooT2G10zAth@cluster0.n0j3tzk.mongodb.net/?retryWrites=true&w=majority";
+
+const port = process.env.PORT || 4400;
 
 const app = express();
 
@@ -11,11 +12,11 @@ app.use(cors());
 app.use(urlencoded({extended:true}));
 app.use(express.json());
 
-app.get(url, (req, res) => {
+app.get('/users', (req, res) => {
 
     Mongodb.connect(connString).then(clientObject => {
 
-        var database = clientObject.db("demo");
+        var database = clientObject.db("for-deployment");
         database.collection("users").find({}).toArray().then(document => {
             res.send(document);
             res.end();
@@ -25,11 +26,11 @@ app.get(url, (req, res) => {
 
 
 
-app.get(url, (req, res) => {
+app.get('/signin', (req, res) => {
 
     Mongodb.connect(connString).then(clientObject => {
 
-        var database = clientObject.db("demo");
+        var database = clientObject.db("for-deployment");
         database.collection("users").find({}).toArray().then(document => {
             res.send(document);
             res.end();
@@ -38,7 +39,7 @@ app.get(url, (req, res) => {
 })
 
 
-app.post(url , async(req, res) => {
+app.post('/signup' , async(req, res) => {
     
     var data = {
         UserId : req.body.UserId,
@@ -51,7 +52,7 @@ app.post(url , async(req, res) => {
 
     Mongodb.connect(connString).then(clientObject => {
 
-        var database = clientObject.db("demo");
+        var database = clientObject.db("for-deployment");
         database.collection("users").insertOne(data).then(document => {
             res.send(document);
             res.end();
@@ -62,8 +63,8 @@ app.post(url , async(req, res) => {
 app.connect((req, res) => {
     res.writeHead(200, {"Content-Type":"application/json"});
     res.end();
-})
+});
 
-app.listen(4500, () => {
-    console.log(`Server started to this URL http://localhost:4500`);
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 })
