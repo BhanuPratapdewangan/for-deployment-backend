3bdfa8a3310f3d9330d86253fc3442dcb8d7183a
bh@nu77 <bhanupratap04123@gmail.com>
2023-09-24 16:51:31 +0530

    mongodb cluster password change

--- db/config.js
+++ db/config.js
@@ -1,4 +1,4 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://bhanupratap04123:x4R6GBn5upv6KG5f@cluster0.n0j3tzk.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://bhanupratap04123:TbRVTASmZImaFNE5@cluster0.n0j3tzk.mongodb.net/?retryWrites=true&w=majority");
 
